c21e9a59b1f423a1f04eb352c7d39d0bb558bead
iptkrisna <krisnamath9@gmail.com>
2019-07-03 16:52:11 +0700

    fix scroll view soon

--- autoLayoutTL/ViewController.swift
+++ autoLayoutTL/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonOutlet: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        buttonOutlet.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 80)
+        buttonOutlet.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 80)
+        buttonOutlet.topAnchor.constraint(equalTo: view.topAnchor, constant: 20)
+        buttonOutlet.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: 20)
+        
     }
 
 
