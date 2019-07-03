f311ebbc2f8144a96184a88fe35ba905e281e06f
iptkrisna <krisnamath9@gmail.com>
2019-07-03 15:00:31 +0700

    create a subclass of uibutton

--- autoLayoutTL/ViewController.swift
+++ autoLayoutTL/ViewController.swift
@@ -15,6 +15,18 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+}
 
+class roundButton: UIButton {
+    override func didMoveToWindow() {
+        self.layer.cornerRadius = self.frame.height/2 //radius of button corner
+        self.backgroundColor = UIColor.gray //color of button
+        self.setTitleColor(UIColor.white, for: .normal) //title of button
+        self.layer.shadowColor = UIColor.yellow.cgColor //color of shadow
+        self.layer.shadowRadius = 10 //radius of shadow, count from button frame size
+        self.layer.shadowOpacity = 0.5 //shadow opacity, alpha of shadow
+        self.layer.shadowOffset = CGSize(width: 0, height: 0) //position of shadow
+        
+    }
 }
 
