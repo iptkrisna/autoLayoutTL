a35a5d98f4be860271f506c36d1bb0a61e3b0df8
iptkrisna <krisnamath9@gmail.com>
2019-07-03 14:22:31 +0700

    Initial Commit

---
+++ autoLayoutTL/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autoLayoutTL
+//
+//  Created by I Putu Krisna on 03/07/19.
+//  Copyright © 2019 I Putu Krisna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
